f37b16d47bd35034d066f1c0aedff9eee13497b3
matiasAAFP <maafierroperez@gmail.com>
2025-01-15 13:12:31 -0300

    siu

---
+++ hola.py
@@ -0,0 +1,6 @@
+hilda=0 
+while hilda !=170000000:
+ print(hilda)
+ hilda=hilda+4
+print("perdiste") 
+print("por que?")
---
+++ wasa.py
@@ -0,0 +1,9 @@
+hilda=2
+print("hilda es igual a")
+print(hilda)
+if hilda    ==2:    print("Hilda es muy pro")
+arbon=7
+print("¿cuantas tortas se comio arbon?")
+print(arbon)
+if arbon    ==7:    print("arbon es un gloton")
+else: print("arbon esta gordo")
